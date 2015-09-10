625612464dcf4c60b534d3c5c8a11454559c0114
shanesully <shanesully01@gmail.com>
2015-09-10 14:08:50 +0100

    Added support for arbitrary number of arguments

--- extract_strings.py
+++ extract_strings.py
@@ -7,22 +7,30 @@
 import sys
 from BeautifulSoup import BeautifulStoneSoup
 
-if len(sys.argv) != 2:
-    print "Incorrect arguments\n\nUsage:\n\n\t$ python {} example.xml\n".format(sys.argv[0])
+if len(sys.argv) <= 1:
+    print "Incorrect arguments\n\nUsage:\n\n\t$ python {} example.xml ...\n".format(sys.argv[0])
     sys.exit()
 
-with open(sys.argv[1]) as xml_source_file:
-    file_name = str(xml_source_file.name).split('.')[0] + "_strings.txt"
+for given_file in sys.argv[1:]:
 
-    with open(file_name, 'w+') as strings_destination_file:
-        soup = BeautifulStoneSoup(xml_source_file)
-        strings = []
+    file_format = str(given_file).split('.')[1] 
 
-        for tag in soup.findAll(['string', 'plurals']):
-           strings.append(tag.text.encode('utf-8'))
+    if file_format == 'xml':
 
-        for string in strings:
-            strings_destination_file.write(string + '\n')
+        with open(given_file) as xml_source_file:
+            strings_file_name = str(xml_source_file.name).split('.')[0] + "_strings.txt"
 
-        print "String count: {}".format(len(strings))
-        print "{} file created".format(file_name)
+            with open(strings_file_name, 'w+') as strings_file:
+                soup = BeautifulStoneSoup(xml_source_file)
+                strings = []
+
+                for tag in soup.findAll(['string', 'plurals']):
+                   strings.append(tag.text.encode('utf-8'))
+
+                for string in strings:
+                    strings_file.write(string + '\n')
+
+                print "String count: {}".format(len(strings))
+                print "{} file created".format(strings_file_name)
+    else:
+        print "Ignoring {} as it is .{} format and not .xml".format(given_file, str(given_file).split('.')[1])
