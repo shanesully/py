71418aef95ef94c9ef7bd554058773b0bd2ca2e8
shanesully <shanesully01@gmail.com>
2015-09-10 11:43:48 +0100

    Improved naming

--- extract_strings.py
+++ extract_strings.py
@@ -11,18 +11,18 @@ if len(sys.argv) != 2:
     print "Incorrect arguments\n\nUsage:\n\n\t$ python {} example.xml\n".format(sys.argv[0])
     sys.exit()
 
-with open(sys.argv[1]) as xml_file:
-    file_name = str(xml_file.name).split('.')[0] + "_strings.txt"
+with open(sys.argv[1]) as xml_source_file:
+    file_name = str(xml_source_file.name).split('.')[0] + "_strings.txt"
 
-    with open(file_name, 'w+') as strings_file:
-        soup = BeautifulStoneSoup(xml_file)
+    with open(file_name, 'w+') as strings_destination_file:
+        soup = BeautifulStoneSoup(xml_source_file)
         strings = []
 
         for tag in soup.findAll(['string', 'plurals']):
            strings.append(tag.text.encode('utf-8'))
 
         for string in strings:
-            strings_file.write(string + '\n')
+            strings_destination_file.write(string + '\n')
 
         print "String count: {}".format(len(strings))
         print "{} file created".format(file_name)
