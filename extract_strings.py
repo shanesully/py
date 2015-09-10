f45d7a90f2acc99bf8280a053124cabc3450d442
shanesully <shanesully01@gmail.com>
2015-09-10 11:25:05 +0100

    Greatly improved string extractor. Renamed methods. Revised control flow

--- extract_strings.py
+++ extract_strings.py
@@ -3,30 +3,26 @@
 # shanesully
 #
 # Extract strings from an xml file
-#
+
 import sys
 from BeautifulSoup import BeautifulStoneSoup
 
-if len(sys.argv) == 1:
-    print "Incorrect arguments\n\nUsage:\n\t$ python extract_strings.py arg1"
+if len(sys.argv) != 2:
+    print "Incorrect arguments\n\nUsage:\n\n\t$ python {} example.xml\n".format(sys.argv[0])
     sys.exit()
 
-# XML strings file
-src = open(sys.argv[1])
-# Strip src file extension and append
-dest_name = str(sys.argv[1][:-4]) + "_strings" + ".txt"
-dest = open(dest_name, 'w+')
+with open(sys.argv[1]) as xml_file:
+    file_name = str(xml_file.name).split('.')[0] + "_strings.txt"
 
-soup = BeautifulStoneSoup(src)
+    with open(file_name, 'w+') as strings_file:
+        soup = BeautifulStoneSoup(xml_file)
+        strings = []
 
-words = []
+        for tag in soup.findAll(['string', 'plurals']):
+           strings.append(tag.text.encode('utf-8'))
 
-for tag in soup.findAll(['string', 'plurals']):
-   words.append(tag.text.encode('utf-8'))
+        for string in strings:
+            strings_file.write(string + '\n')
 
-# Populate names file
-for word in words:
-    dest.write(word + '\n')
-
-print "Word count: {}".format(len(words))
-print "{} file created".format(dest_name)
+        print "String count: {}".format(len(strings))
+        print "{} file created".format(file_name)
