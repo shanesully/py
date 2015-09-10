8da78c2b9d55c0d51e8acd33c1dc48fe31a8ce38
shanesully <shanesully01@gmail.com>
2015-09-10 16:54:33 +0100

    Updated docs. Added prelim dict builder func

--- extract_strings.py
+++ extract_strings.py
@@ -2,18 +2,20 @@
 # 
 # shanesully
 #
-# Extract strings from an xml file
+# Extract strings from given xml files
+#
 
 import sys
 from BeautifulSoup import BeautifulStoneSoup
 
 if len(sys.argv) <= 1:
-    print "\n\nUsage:\n\n\t$ python {} example.xml ...\n".format(sys.argv[0])
+    print "\nUsage:\n\n\t$ python {} example.xml ...\n".format(sys.argv[0])
     sys.exit()
+
 
 def create_basic_strings_file(files):
     """
-    Creates a new file of newline-terminated strings based on the string
+    Create a file of newline-terminated strings based on string
     attribute tags for every given xml file
     """
     for given_file in files:
@@ -37,9 +39,33 @@ def create_basic_strings_file(files):
         else:
             print "Ignoring {} as it is in .{} format and not .xml".format(given_file, str(given_file).split('.')[1])
 
+
+def create_multi_lang_dict(files):
+    """
+    Return a dict of string attribs and values for every given xml file
+    """
+    strings_dict = {}
+
+    for given_file in files:
+        with open(given_file) as xml_source_file:
+            soup = BeautifulStoneSoup(xml_source_file)
+
+            for tag in soup.findAll(['string', 'plurals']):
+
+                if str(tag['name']) in strings_dict:
+                    strings_dict[str(tag['name'])].append(tag.text.encode('utf-8'))
+                else:
+                    strings_dict[str(tag['name'])] = [ tag.text.encode('utf-8') ]
+
+    return strings_dict
+
+
 def main():
     if sys.argv[1] == '-s':
         create_basic_strings_file(sys.argv[2:])
+    elif sys.argv[1] == '-k':
+        create_multi_lang_dict(sys.argv[2:])
+
 
 if __name__ == '__main__':
     main()
