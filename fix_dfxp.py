ea845af9d212c73701d2738a413a11347cc173fe
shanesully <shanesully01@gmail.com>
2015-09-18 15:21:22 +0100

    Initial commit(skeleton) for dfxp file fixer script

---
+++ fix_dfxp.py
@@ -0,0 +1,23 @@
+'''
+@shanesully
+Script to change the extension and fix the content of borked
+DFXP subtitle translation XML files
+'''
+
+class dfxp_fix(object):
+
+    def __init__(self):
+        pass
+
+    def fix_extension(self):
+        '''
+        Change the extension of a given file from whatever it was before
+        to plain DFXP
+        '''
+
+        pass
+
+    def fix_structure(self):
+        # Parse and fix missing newlines and broken file structure
+        pass
+
