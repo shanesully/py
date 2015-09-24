d673cb0daa5d94df856eb3f2f8b3554b8e58676c
shanesully <shanesully01@gmail.com>
2015-09-24 15:23:16 +0100

    Added bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,26 @@
+# shanesully
+# Bubble Sort numbers in-place
+
+import sys
+
+def bubble_sort(numbers):
+    i = 0
+    j = 0
+
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - 1 - i):
+            if numbers[j] > numbers[j + 1]:
+                temp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp
+    
+    return numbers
+
+def main():
+    numbers = [int(x) for x in sys.argv[1:]]
+
+    print bubble_sort(numbers)
+
+if __name__ == '__main__':
+    main()
+
