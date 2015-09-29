6719ee985b8cc7c4151f7927232a1fce6d439b2a
sully <shanesully01@gmail.com>
2015-09-29 12:24:18 +0100

    Added caeser cipher script

---
+++ bash/caeser_cipher.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# shanesully 
+#
+# Caeser Cipher Encryption Script
+#   
+
+function print_usage {
+    # Print commandline usage info
+    printf "\nProgram: \n\t$0\n"
+    printf "\nUsage:\n"
+    printf "\t$ $0 $args\n"
+    printf "\nNotes:\n"
+    printf "\t- Run with or without arguments\n"
+    printf "\n"
+}
+
+function caeser_encrypt {
+    # Encrypt using Caeser Cipher 
+    local ciphertext=`echo $1 | tr 'A-Za-z' 'X-ZA-Wx-za-w'`
+    printf $ciphertext
+}
+
+function caeser_decrypt {
+    # Reverse Caeser Cipher
+    local plaintext=`echo $1 | tr 'X-ZA-Wx-za-w' 'A-Za-z'`
+    printf $plaintext
+}
+
+function format_content {
+    # Pretty-print cipher process
+    printf "Original Message:\n$1\n"
+    printf "Encrypted Message:\n$2\n"
+    printf "Decrypted Message:\n$3\n"
+    printf "\n"
+}
+
+function run_cipher_process {
+    # Process messages
+    original_msg=$1
+    ciphertext=$(caeser_encrypt $original_msg)
+    plaintext=$(caeser_decrypt $ciphertext)
+
+    # Display content
+    format_content $original_msg $ciphertext $plaintext
+}
+
+function get_new_msg {
+    # Get message from user
+    echo "Enter message or (q)uit?"
+    read anwser
+
+    if [ $anwser = "q" ]; then
+        exit
+    else
+        printf ""
+        # Run cipher process on message 
+        run_cipher_process $anwser 
+    fi
+}
+
+# If no args passed
+if [ $# -eq 0 ]; then
+    # Print program usage for future sessions
+    print_usage
+fi
+
+# Encrypt messages from commandline
+for a in ${BASH_ARGV[*]}; do
+    run_cipher_process $a
+done
+
+# Encrypt message from user until
+# they choose to quit
+while :
+do
+    get_new_msg 
+done
